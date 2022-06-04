a597512a04f5a71db33d51605d23d3563fb1ea1f
RyzenMu <muruga13in2022@gmail.com>
2022-06-04 19:10:33 +0530

    created a 2D array in data distribution

--- random_Data_distribution.py
+++ random_Data_distribution.py
@@ -5,3 +5,11 @@ import numpy as np
 random_matrix = np.random.choice([3, 5, 7, 9], p = [0.1, 0.3, 0.6, 0.0], size=(100))
 
 print(random_matrix)
+print('------------------------------------------------------')
+
+# creating a 2D array using random and choice
+
+random_2D_matrix = np.random.choice([3, 4, 6, 9], p=[0.2, 0.5, 0.3, 0.0], size=(3, 5))
+print(random_2D_matrix)
+
+print('----------------------------------------------------------------------------------')
